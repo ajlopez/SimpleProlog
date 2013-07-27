7db22172b18d01b543bf119e53cb85b0f64923a8
ajlopez <webmaster@ajlopez.com>
2013-07-27 20:38:33 -0300

    Variable first tests

---
+++ test/variable.js
@@ -0,0 +1,16 @@
+
+var variable = require('../lib/variable'),
+    assert = require('assert');
+    
+// variable as function
+
+assert.ok(variable);
+assert.equal(typeof variable, 'function');
+
+// create variable
+
+var result = variable('X');
+assert.ok(result);
+assert.equal(result.name, 'X');
+assert.equal(result.asString(), 'X');
+
