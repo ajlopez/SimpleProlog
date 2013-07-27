8107e89ee333f6a38bb8aa01cddffca38a879a8c
ajlopez <webmaster@ajlopez.com>
2013-07-27 20:02:48 -0300

    Atom match, first test

--- test/atom.js
+++ test/atom.js
@@ -14,3 +14,36 @@ assert.ok(atomfoo);
 assert.equal(atomfoo.name, 'foo');
 assert.equal(atomfoo.asString(), 'foo');
 
+// match atom with same name
+
+var atomfoo2 = atom('foo');
+assert.ok(atomfoo.match(atomfoo2));
+
+// no match atom with other name
+
+var atombar = atom('bar');
+assert.ok(!atomfoo.match(atombar));
+
+// no match with number
+
+assert.ok(!atomfoo.match(42));
+
+// no match with string
+
+assert.ok(!atomfoo.match("foo"));
+
+// no match with null
+
+assert.ok(!atomfoo.match(null));
+
+// no match with undefined
+
+assert.ok(!atomfoo.match(null));
+
+// no match with false
+
+assert.ok(!atomfoo.match(false));
+
+// no match with true
+
+assert.ok(!atomfoo.match(true));
