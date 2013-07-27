5de0db8c85900c742ffb4457adb56315357c88a1
ajlopez <webmaster@ajlopez.com>
2013-07-27 19:35:52 -0300

    Fact first test

---
+++ test/fact.js
@@ -0,0 +1,19 @@
+
+var fact = require('../lib/fact'),
+    atom = require('../lib/atom'),
+    assert = require('assert');
+    
+// fact as a function
+
+assert.ok(fact);
+assert.equal(typeof fact, 'function');
+
+// fact with arguments
+
+var result = fact(atom('a'), [1, 2]);
+assert.ok(result);
+assert.ok(result.functor);
+assert.equal(result.functor.asString(), 'a');
+assert.equal(result.arity, 2);
+assert.equal(result.asString(), 'a(1, 2)');
+
