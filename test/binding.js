b6fe426a63d2509c05b53787c79b028e4d95eb06
ajlopez <webmaster@ajlopez.com>
2013-07-27 20:49:55 -0300

    Undefined in binding is null

--- test/binding.js
+++ test/binding.js
@@ -14,3 +14,8 @@ assert.ok(result);
 assert.equal(result.current(), 2);
 assert.equal(result.get(0), null);
 assert.equal(result.get(1), null);
+
+// strict null
+
+assert.strictEqual(result.get(0), null);
+assert.strictEqual(result.get(1), null);
