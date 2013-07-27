4e6486b472b2334e227c4ddbe0abe4c4465b983d
ajlopez <webmaster@ajlopez.com>
2013-07-27 19:36:50 -0300

    Additional test for fact

--- test/fact.js
+++ test/fact.js
@@ -17,3 +17,12 @@ assert.equal(result.functor.asString(), 'a');
 assert.equal(result.arity, 2);
 assert.equal(result.asString(), 'a(1, 2)');
 
+// fact with atoms as arguments
+
+var result = fact(atom('a'), [atom('b'), atom('c'), atom('d')]);
+assert.ok(result);
+assert.ok(result.functor);
+assert.equal(result.functor.asString(), 'a');
+assert.equal(result.arity, 3);
+assert.equal(result.asString(), 'a(b, c, d)');
+
