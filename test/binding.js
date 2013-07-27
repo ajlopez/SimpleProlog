a21eec51f91d17694e306721851d7c7a1dc2f07f
ajlopez <webmaster@ajlopez.com>
2013-07-27 20:48:32 -0300

    Binding first test

---
+++ test/binding.js
@@ -0,0 +1,16 @@
+
+var binding = require('../lib/binding'),
+    assert = require('assert');
+    
+// binding as a function
+
+assert.ok(binding);
+assert.equal(typeof binding, 'function');
+
+// create binding
+
+var result = binding(2);
+assert.ok(result);
+assert.equal(result.current(), 2);
+assert.equal(result.get(0), null);
+assert.equal(result.get(1), null);
