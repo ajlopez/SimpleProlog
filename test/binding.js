3e8808584545b8e9b75e8ddec29fbb20500dce0c
ajlopez <webmaster@ajlopez.com>
2013-07-27 21:10:14 -0300

    Binding grow first test

--- test/binding.js
+++ test/binding.js
@@ -59,3 +59,15 @@ assert.throws(
     },
     Error
 );
+
+// grows
+
+result.grow(3);
+
+assert.equal(result.current(), 5);
+assert.equal(result.get(0), 1);
+assert.equal(result.get(1), "foo");
+assert.strictEqual(result.get(2), null);
+assert.strictEqual(result.get(3), null);
+assert.strictEqual(result.get(4), null);
+
