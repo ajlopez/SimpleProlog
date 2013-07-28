e745c6f755a7b4e1b07ad02e4a28d4773012c806
ajlopez <webmaster@ajlopez.com>
2013-07-28 07:29:09 -0300

    Base assert and query simple facts wo/variables

--- test/base.js
+++ test/base.js
@@ -12,5 +12,16 @@ assert.equal(typeof base, 'function');
 // query an empty base
 
 var base1 = base();
-assert.equal(base1.query(fact(atom('a'))), false);
+var fact1 = fact(atom('a'));
+assert.equal(base1.query(fact1), false);
+
+// assert and query
+
+base1.assert(fact1);
+assert.equal(base1.query(fact1), true);
+
+// query unknown fact
+
+var fact2 = fact(atom('b'), [1, 2]);
+assert.equal(base1.query(fact2), false);
 
