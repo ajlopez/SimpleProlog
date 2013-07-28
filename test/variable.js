2ba082ef8d05224f24f8f9fd20b75004133a6076
ajlopez <webmaster@ajlopez.com>
2013-07-27 21:21:13 -0300

    Variable first match, with test

--- test/variable.js
+++ test/variable.js
@@ -1,5 +1,7 @@
 
 var variable = require('../lib/variable'),
+    binding = require('../lib/binding'),
+    atom = require('../lib/atom'),
     assert = require('assert');
     
 // variable as function
@@ -24,4 +26,17 @@ assert.equal(result.name, 'X');
 assert.equal(result.asString(), 'X');
 assert.equal(result.variable, true);
 assert.equal(result.offset, 10);
+
+// unbound variable match number
+
+var varx = variable('X', 0);
+assert.ok(varx);
+assert.strictEqual(varx.offset, 0);
+var bind = binding(1);
+var atomx = atom('x');
+assert.equal(varx.match(atomx, bind), true);
+
+var result = bind.get(0);
+assert.ok(result);
+assert.strictEqual(result, atomx);
 
