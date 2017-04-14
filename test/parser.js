e778f03ee86ce46b5523a43efd147a66fe4f8dd5
ajlopez <webmaster@ajlopez.com>
2017-04-14 12:08:21 -0300

    Refactor structure arguments to use a function

--- test/parser.js
+++ test/parser.js
@@ -63,10 +63,10 @@ exports['parse structure with arity 1'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), "a:1");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 1);
-    test.equal(result.args[0], 1);
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 1);
+    test.equal(result.arguments()[0], 1);
 }
 
 exports['parse structure with arity 2'] = function (test) {
@@ -80,11 +80,11 @@ exports['parse structure with arity 2'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), "a:2");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 2);
-    test.equal(result.args[0], 1);
-    test.equal(result.args[1], 2);
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 2);
+    test.equal(result.arguments()[0], 1);
+    test.equal(result.arguments()[1], 2);
 }
 
 exports['parse and structure with two atoms'] = function (test) {
@@ -98,11 +98,11 @@ exports['parse and structure with two atoms'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), ",:2");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 2);
-    test.equal(result.args[0].name(), 'a');
-    test.equal(result.args[1].name(), 'b');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 2);
+    test.equal(result.arguments()[0].name(), 'a');
+    test.equal(result.arguments()[1].name(), 'b');
 }
 
 exports['parse and structure with two structures'] = function (test) {
@@ -116,11 +116,11 @@ exports['parse and structure with two structures'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), ",:2");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 2);
-    test.equal(result.args[0].signature(), 'a:1');
-    test.equal(result.args[1].signature(), 'b:2');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 2);
+    test.equal(result.arguments()[0].signature(), 'a:1');
+    test.equal(result.arguments()[1].signature(), 'b:2');
 }
 
 exports['parse and structure with three structures'] = function (test) {
@@ -134,12 +134,12 @@ exports['parse and structure with three structures'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), ",:3");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 3);
-    test.equal(result.args[0].signature(), 'a:1');
-    test.equal(result.args[1].signature(), 'b:2');
-    test.equal(result.args[2].signature(), 'c:3');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 3);
+    test.equal(result.arguments()[0].signature(), 'a:1');
+    test.equal(result.arguments()[1].signature(), 'b:2');
+    test.equal(result.arguments()[2].signature(), 'c:3');
 }
 
 exports['parse simple rule'] = function (test) {
@@ -153,11 +153,11 @@ exports['parse simple rule'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), ":-:2");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 2);
-    test.equal(result.args[0].name(), 'a');
-    test.equal(result.args[1].name(), 'b');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 2);
+    test.equal(result.arguments()[0].name(), 'a');
+    test.equal(result.arguments()[1].name(), 'b');
 }
 
 exports['parse rule with and body'] = function (test) {
@@ -171,11 +171,11 @@ exports['parse rule with and body'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), ":-:2");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 2);
-    test.equal(result.args[0].name(), 'a');
-    test.equal(result.args[1].signature(), ',:2');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 2);
+    test.equal(result.arguments()[0].name(), 'a');
+    test.equal(result.arguments()[1].signature(), ',:2');
 }
 
 exports['parse simple query'] = function (test) {
@@ -189,10 +189,10 @@ exports['parse simple query'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), "?-:1");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 1);
-    test.equal(result.args[0].name(), 'a');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 1);
+    test.equal(result.arguments()[0].name(), 'a');
 }
 
 exports['parse query with and body'] = function (test) {
@@ -206,10 +206,10 @@ exports['parse query with and body'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), "?-:1");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 1);
-    test.equal(result.args[0].signature(), ',:2');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 1);
+    test.equal(result.arguments()[0].signature(), ',:2');
 	
 	test.equal(myparser.parse(), null);
 }
@@ -225,10 +225,10 @@ exports['parse query with and body and point'] = function (test) {
     test.equal(result.nvariables(), 0);
     test.equal(result.nanonymous(), 0);
     test.equal(result.signature(), "?-:1");
-    test.ok(result.args);
-    test.ok(Array.isArray(result.args));
-    test.equal(result.args.length, 1);
-    test.equal(result.args[0].signature(), ',:2');
+    test.ok(result.arguments());
+    test.ok(Array.isArray(result.arguments()));
+    test.equal(result.arguments().length, 1);
+    test.equal(result.arguments()[0].signature(), ',:2');
 	
 	test.equal(myparser.parse(), null);
 }
