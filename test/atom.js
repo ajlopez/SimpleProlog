3865b8fe164f4112a2bc87499d56b58d3c04405c
ajlopez <webmaster@ajlopez.com>
2017-04-17 14:50:53 -0300

    Atom function in atom

--- test/atom.js
+++ test/atom.js
@@ -14,6 +14,7 @@ exports['create atom with name'] = function (test) {
     test.equal(atomfoo.name(), 'foo');
     test.equal(atomfoo.asString(), 'foo');
 	test.equal(atomfoo.variable(), false);
+	test.equal(atomfoo.atom(), true);
 }
 
 exports['match atom with same name'] = function (test) {
