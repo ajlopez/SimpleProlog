1860ab914253f1e679903670c53bb44b91919cac
ajlopez <webmaster@ajlopez.com>
2020-02-21 08:14:02 -0300

    Atom tests refactor

--- test/atom.js
+++ test/atom.js
@@ -1,15 +1,17 @@
 
-var atom = require('../lib/atom'),
-    binding = require('../lib/binding'),
-    variable = require('../lib/variable');
+const atom = require('../lib/atom');
+const binding = require('../lib/binding');
+const variable = require('../lib/variable');
     
 exports['atom as a function'] = function (test) {
     test.ok(atom);
+    
     test.equal(typeof atom, 'function');
 }
 
 exports['create atom with name'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
+    
     test.ok(atomfoo);
     test.equal(atomfoo.name(), 'foo');
     test.equal(atomfoo.asString(), 'foo');
@@ -19,55 +21,63 @@ exports['create atom with name'] = function (test) {
 }
 
 exports['match atom with same name'] = function (test) {
-    var atomfoo = atom('foo');
-    var atomfoo2 = atom('foo');
+    const atomfoo = atom('foo');
+    const atomfoo2 = atom('foo');
+    
     test.ok(atomfoo.match(atomfoo2));
 }
 
 exports['no match atom with other name'] = function (test) {
-    var atomfoo = atom('foo');
-    var atombar = atom('bar');
+    const atomfoo = atom('foo');
+    const atombar = atom('bar');
+    
     test.ok(!atomfoo.match(atombar));
 }
 
 exports['no match with number'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
+    
     test.ok(!atomfoo.match(42));
 }
 
 exports['no match with string'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
+    
     test.ok(!atomfoo.match("foo"));
 }
 
 exports['no match with null'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
 
     test.ok(!atomfoo.match(null));
 }
 
 exports['no match with undefined'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
+    
     test.ok(!atomfoo.match(null));
 }
 
 exports['no match with false'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
+    
     test.ok(!atomfoo.match(false));
 }
 
 exports['no match with true'] = function (test) {
-    var atomfoo = atom('foo');
+    const atomfoo = atom('foo');
+    
     test.ok(!atomfoo.match(true));
 }
 
 exports['match with variable'] = function (test) {
-    var atomfoo = atom('foo');
-    var bindings = binding(1);
-    var varx = variable('X', 0);
+    const atomfoo = atom('foo');
+    const bindings = binding(1);
+    const varx = variable('X', 0);
     varx.offset = 0;
+    
     test.ok(atomfoo.match(varx, bindings));
-    var value = bindings.get(0);
+    const value = bindings.get(0);
     test.ok(value);
     test.equal(value.name(), 'foo');
 }
