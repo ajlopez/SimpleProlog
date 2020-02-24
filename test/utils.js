85f330fe09591731696b4edcab818eb3b2e050c9
ajlopez <webmaster@ajlopez.com>
2020-02-24 09:35:20 -0300

    Utils tests refactor

--- test/utils.js
+++ test/utils.js
@@ -1,50 +1,59 @@
-var utils = require('../lib/utils');
-var variable = require('../lib/variable');
-var atom = require('../lib/atom');
-var structure = require('../lib/structure');
+const utils = require('../lib/utils');
+const variable = require('../lib/variable');
+const atom = require('../lib/atom');
+const structure = require('../lib/structure');
 
 exports['variable is a variable'] = function (test) {
-	var var1 = variable('X');
+	const var1 = variable('X');
+    
 	test.ok(utils.isVariable(var1));
 };
 
 exports['atom is not a variable'] = function (test) {
-	var atom1 = atom('a');
+	const atom1 = atom('a');
+    
 	test.equal(utils.isVariable(atom1), false);
 };
 
 exports['structure is not a variable'] = function (test) {
-	var structure1 = structure(atom('a'), variable('X'));;
+	const structure1 = structure(atom('a'), variable('X'));;
+    
 	test.equal(utils.isVariable(structure1), false);
 };
 
 exports['variable is not an atom'] = function (test) {
-	var var1 = variable('X');
+	const var1 = variable('X');
+    
 	test.equal(utils.isAtom(var1), false);
 };
 
 exports['atom is an atom'] = function (test) {
-	var atom1 = atom('a');
+	const atom1 = atom('a');
+    
 	test.equal(utils.isAtom(atom1), true);
 };
 
 exports['structure is not an atom'] = function (test) {
-	var structure1 = structure(atom('a'), variable('X'));;
+	const structure1 = structure(atom('a'), variable('X'));;
+    
 	test.equal(utils.isAtom(structure1), false);
 };
 
 exports['variable is not an structure'] = function (test) {
-	var var1 = variable('X');
+	const var1 = variable('X');
+    
 	test.equal(utils.isStructure(var1), false);
 };
 
 exports['atom is not an structure'] = function (test) {
-	var atom1 = atom('a');
+	const atom1 = atom('a');
+    
 	test.equal(utils.isStructure(atom1), false);
 };
 
 exports['structure is an structure'] = function (test) {
-	var structure1 = structure(atom('a'), variable('X'));;
+	const structure1 = structure(atom('a'), variable('X'));;
+    
 	test.equal(utils.isStructure(structure1), true);
 };
 
