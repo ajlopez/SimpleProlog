2089d7bfeabb325e0a67d1d62faba3e64e718a53
ajlopez <webmaster@ajlopez.com>
2020-05-28 16:49:35 -0300

    Variable factory method in library entrypoint

--- test/simpleprolog.js
+++ test/simpleprolog.js
@@ -28,3 +28,21 @@ exports['query unknown structure'] = function (test) {
     test.equal(base.query(structure), false);
 }
 
+exports['query using a variable'] = function (test) {
+    const base = sp.base();
+    const structure1 = sp.structure(sp.atom('a'), sp.atom('b'));
+    const structure2 = sp.structure(sp.atom('a'), sp.atom('c'));
+    
+    base.assert(structure1);
+    base.assert(structure2);
+
+    const structurex = sp.structure(sp.atom('a'), sp.variable('X'));
+
+    const result = base.query(structurex);
+
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.ok(result.X);
+    test.equal(result.X.name(), 'b');
+}
+
