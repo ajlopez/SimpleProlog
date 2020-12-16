38881b045470ed30c24613ecff1c194397cdfec6
ajlopez <webmaster@ajlopez.com>
2020-12-16 08:26:20 -0300

    Parse variable using gepars

--- test/parsers.js
+++ test/parsers.js
@@ -12,3 +12,38 @@ exports['parse atom'] = function (test) {
     test.equal(parser.parse('atom'), null);
 };
 
+exports['parse atom with spaces'] = function (test) {
+    const parser = parsers.parser('  a   ');
+    
+    const result = parser.parse('atom');
+    
+    test.ok(result);
+    test.equal(result.name(), 'a');
+    
+    test.equal(parser.parse('atom'), null);
+};
+
+exports['parse variable'] = function (test) {
+    const parser = parsers.parser('X');
+    
+    const result = parser.parse('variable');
+    
+    test.ok(result);
+    test.ok(result.variable);
+    test.equal(result.name(), 'X');
+    
+    test.equal(parser.parse('variable'), null);
+};
+
+exports['parse variable with spaces'] = function (test) {
+    const parser = parsers.parser('  X   ');
+    
+    const result = parser.parse('variable');
+    
+    test.ok(result);
+    test.ok(result.variable);
+    test.equal(result.name(), 'X');
+    
+    test.equal(parser.parse('variable'), null);
+};
+
